af2d873f586f728191a3b9a449e74d95c88447c5
Jeff Fischer - EPiServer <46690775+jefffischer-episerver@users.noreply.github.com>
2020-03-26 08:45:35 -0400

    Feature/readme enhancements (#3)
    * Adding extension method for rectifying Grace dependencies.
    
    * Added a few readme notes.

---
+++ src/EpiserverIoc.Locators.Grace/Extensions/ServiceFixExtensions.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Data;
+using EPiServer.Data.Internal;
+using EPiServer.Data.Providers;
+using EPiServer.ServiceLocation;
+
+namespace EpiserverIoc.Locators.Grace.Extensions
+{
+    public static class ServiceFixExtensions
+    {
+        public static void ResolveGraceDependencyIssues(this IServiceConfigurationProvider services)
+        {
+            services.RemoveAll<IAsyncDatabaseExecutorFactory>();
+            services.RemoveAll<ServiceAccessor<IAsyncDatabaseExecutorFactory>>();
+
+            //services.Forward<IDatabaseExecutorFactory, IAsyncDatabaseExecutorFactory>(); // this is bad, must be a singleton....
+            services.AddSingleton(locator => (IAsyncDatabaseExecutorFactory)locator.GetInstance<IDatabaseExecutorFactory>());
+            services.AddTransient(s => s.GetInstance<IAsyncDatabaseExecutorFactory>().CreateDefaultHandler());
+            services.AddTransient(s => (ServiceAccessor<IAsyncDatabaseExecutor>)(() => s.GetInstance<IAsyncDatabaseExecutor>()));
+        }
+    }
+}
