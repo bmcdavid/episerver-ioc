7ad93625f702c1fe9eb447ebf8eba2b994be5c30
Jeff Fischer <Jeff.Fischer@episerver.com>
2020-04-02 17:30:02 -0400

    Adding structure map chnages.

---
+++ src/EpiserverIoc.Locators.StructureMap/StructureMapContainerFactory.cs
@@ -0,0 +1,19 @@
+﻿using EpiserverIoc.Core;
+using EpiserverIoc.Locators.DryIoc;
+using EPiServer.ServiceLocation;
+using StructureMap;
+
+[assembly: AbstractLocatorFactoryCreator(typeof(StructureMapContainerFactory), nameof(StructureMapContainerFactory.Create))]
+
+namespace EpiserverIoc.Locators.DryIoc
+{
+    public static class StructureMapContainerFactory
+    {
+        public static IServiceLocator Create()
+        {
+            var registry = new Registry();
+
+            return new StructureMapServiceLocator(new Container(registry));
+        }
+    }
+}
---
+++ src/EpiserverIoc.Locators.StructureMap/StructureMapServiceLocatorScoped.cs
@@ -0,0 +1,46 @@
+﻿using EpiserverIoc.Abstractions;
+using EpiserverIoc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EpiserverIoc.Locators.DryIoc
+{
+    public class StructureMapServiceLocatorScoped : BaseScopedServiceLocator, IServiceLocatorScoped
+    {
+        private readonly IResolverContext _resolverContext;
+
+        public StructureMapServiceLocatorScoped(IResolverContext resolverContext, IServiceLocatorScoped parentScope)
+        {
+            _resolverContext = resolverContext;
+            ParentScope = parentScope;
+        }
+
+        public IServiceLocatorScoped ParentScope { get; }
+
+        public IServiceLocatorScoped CreateScope() => RegisterAndReturnScope(new StructureMapServiceLocatorScoped(_resolverContext.OpenScope(), this));
+
+        public void Dispose()
+        {
+            if (_resolverContext is null || _resolverContext.IsDisposed) { return; }
+
+            _resolverContext.Dispose();
+            Dispose(disposing: true);
+        }
+
+        public IEnumerable<object> GetAllInstances(Type serviceType) => _resolverContext.ResolveMany(serviceType).ToList();
+
+        public object GetInstance(Type serviceType) => _resolverContext.Resolve(serviceType, IfUnresolved.Throw);
+
+        public TService GetInstance<TService>() => (TService)GetInstance(typeof(TService));
+
+        public object GetService(Type serviceType) => GetInstance(serviceType);
+
+        public bool TryGetExistingInstance(Type serviceType, out object instance)
+        {
+            instance = _resolverContext.Resolve(serviceType, ifUnresolved: IfUnresolved.ReturnDefaultIfNotRegistered);
+
+            return instance is object;
+        }
+    }
+}
